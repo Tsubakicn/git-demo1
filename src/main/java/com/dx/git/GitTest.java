88d67371b23d64a6d893c53309690419300c50af
gcf <1377581977@qq.com>
2022-11-10 16:50:08 +0800

    second commit

--- src/main/java/com/dx/git/GitTest.java
+++ src/main/java/com/dx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello IDEA Git");
+        System.out.println("Hello IDEA Git1");
     }
 }
