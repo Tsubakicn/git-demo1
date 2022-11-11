34b703632dc35d7d51e18c4e7801f91f475e99e2
gcf <1377581977@qq.com>
2022-11-11 08:26:37 +0800

    push commit

--- src/main/java/com/dx/git/GitTest.java
+++ src/main/java/com/dx/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello IDEA Git2");
         System.out.println("Hello IDEA");
         System.out.println("Hello IDEA Git3");
+        System.out.println("push Hello GitHub");
     }
 }
